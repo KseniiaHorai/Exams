dce5997d8767329156017fdeb6511c425f52aeaf
notnuff <113949291+notnuff@users.noreply.github.com>
2023-01-10 20:09:20 +0200

    improvements
     it is better to put long expressions or expressions that are used several times in separate identifiers

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -2,18 +2,20 @@
 'use strict';
 
 const isValid = (name) => {
-    if (typeof(name) !== 'string' || name.trim() === '' || !name.includes(' ')) {
-        return false;
-    }
-  
-    for (let letter of name) {
-      if (letter === ' ') continue;
-      if ( letter.toLowerCase().charCodeAt(0) < 97 || letter.toLowerCase().charCodeAt(0) > 122) {
-          return false;
-      }
-    }
-    
-    return true;
+  const isString = (typeof(name) !== 'string' ||
+      name.trim() === '' ||
+      !name.includes(' ')
+  );
+  if (isString) return false;
+
+  for (const letter of name) {
+    if (letter === ' ') continue;
+    const letterEncoding = letter.toLowerCase().charCodeAt(0);
+    const isNameCorrect = letterEncoding < 97 || letterEncoding > 122;
+    if (isNameCorrect) return false;
+  }
+
+  return true;
 };
 
 require('../Tests/validate.js')(isValid);
