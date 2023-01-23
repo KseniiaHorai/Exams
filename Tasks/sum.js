3a7f6bc61d84fa6e5d0cd71b037f4ce8ddae8e5d
KseniiaHorai <117169759+KseniiaHorai@users.noreply.github.com>
2023-01-23 20:02:46 +0200

    Fixed

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -1,23 +1,23 @@
 // Sum all numbers from an array
-'use strict';
+"use strict";
 
 const sumNumbers = (array) => {
   let sum = 0;
   if (!Array.isArray(array)) {
-    return 'It is not an array';
+    return "It is not an array";
   }
 
   if (array.length === 0) {
     return 0;
   }
-  
+
   for (let item of array) {
-    if (typeof(item) === 'number') {
-        sum += item; 
+    if (typeof item === "number") {
+      sum += item;
     }
   }
-  
-return sum;
+
+  return sum;
 };
 
-require('../Tests/sum.js')(sumNumbers);
+require("../Tests/sum.js")(sumNumbers);
