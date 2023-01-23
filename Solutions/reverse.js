297ffcf5e7d57606dcf90d1ccad25fb527e80bea
KseniiaHorai <117169759+KseniiaHorai@users.noreply.github.com>
2023-01-23 19:15:52 +0200

    Code fixed

--- Solutions/reverse.js
+++ Solutions/reverse.js
@@ -1,14 +1,13 @@
 // Reverse dict, exchange keys and values
-'use strict';
+"use strict";
 
 const reverseObject = (object) => {
-    const keys = Object.keys(object);
-    for (let item of keys) {
-        const newKey = object[item];
-        object[newKey] = item;
-        delete object[item];
-    }
-    return object;
-
+  const newObject = {};
+  const keys = Object.keys(object);
+  for (let item of keys) {
+    const newKey = object[item];
+    newObject[newKey] = item;
+  }
+  return newObject;
 };
-  require('../Tests/reverse.js')(reverseObject);
+require("../Tests/reverse.js")(reverseObject);
